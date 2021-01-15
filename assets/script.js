21a02fcf38d0607222c731f2ea511b8f1d4692c5
einalem4 <melanie@codehangar.io>
2021-01-14 21:52:02 -0700

    set up quiz landing page

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+
+var questions = [{
+    title: "Which of the following type of variable is visible everywhere in your JavaScript code?",
+    choices: ["Global variable", "Local variable", "All of the above", "None of the above"],
+    answer: "Global Variable"
+},
+{
+    title: "What are variables used for in JavaScript Programs?",
+    choices: ["Varying randomly", "Causing high-school algebra flashbacks", "Storing numbers, dates, or other values", "None of the above"],
+    answer: "Storing numbers, dates, or other values"
+},
+{
+    title: " ______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
+    choices: ["<BODY>", "<SCRIPT>", "<HEAD>", "<TITLE>"],
+    answer: "<SCRIPT>"
+},
+{
+    title: "What is the correct JavaScript syntax to write 'Hello World'?",
+    choices: ["System.out.println('Hello World')", "document.write('Hello World')", "response.write('Hello World')", "println ('Hello World')"],
+    answer: "document.write('Hello World')"
+},
+]
+
+
+//starts the quiz
